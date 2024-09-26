cabbde8e5e33a6f058f0834753b124bb0d7a32b5
cheese6104 <1sheli1nasimov1@gmail.com>
2024-09-26 11:27:57 +0300

    comm bad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,19 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
-import frc.DELib.Subsystems.ServoSubsystem.Base.Motor.ServoSubsystemTalon;
 import frc.DELib.Subsystems.Swerve.SwerveSubsystem;
-import frc.DELib.Subsystems.Swerve.SwerveCommands.RotateToTarget;
+import frc.DELib.Subsystems.Swerve.SwerveCommands.ResetSwerveModules;
 import frc.DELib.Subsystems.Swerve.SwerveCommands.TeleopDrive;
 import frc.DELib.Subsystems.Vision.VisionSubsystem;
 import frc.DELib.Subsystems.Vision.VisionUtil.CameraSettings;
+import frc.DELib.Util.DriverStationController;
+import frc.DELib.Util.SwerveAutoBuilder;
 import frc.robot.commands.ArmCommands.ArmChangeNeutralMode;
 import frc.robot.commands.ArmCommands.ArmHoming;
 import frc.robot.commands.IntagrationCommands.Preset;
+import frc.robot.commands.IntakeCommnands.IntakeEatNote;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -31,44 +35,29 @@ import frc.robot.subsystems.ShooterSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  private CommandPS5Controller controller = new CommandPS5Controller(0);
+  private CommandPS5Controller drivercontroller = new CommandPS5Controller(0);
+  private CommandPS5Controller operatorController = new CommandPS5Controller(1);
+  private DriverStationController driverStationController = new DriverStationController(2);
   private SwerveSubsystem m_swerve;
   private IntakeSubsystem m_intakeSub;
   private ArmSubsystem m_arm;
   private ShooterSubsystem m_shooter;
   private VisionSubsystem m_vision;
-  // private PhoneixSysid sysid;
-  // private SwerveAutoBuilder swerveAutoBuilder;
+  private SwerveAutoBuilder swerveAutoBuilder;
+
   public RobotContainer() {
     m_swerve = SwerveSubsystem.createInstance(Constants.Swerve.swerveConstants);
     m_intakeSub = IntakeSubsystem.getInstance();
     m_arm = new ArmSubsystem(Constants.arm.configuration);
     m_shooter = new ShooterSubsystem(Constants.Shooter.configuration);
-    m_vision = new VisionSubsystem(new CameraSettings(0, 0, 0, 0, 0, 0, true), new CameraSettings(0, 0, 0, 0, 0, 0, false));
-    // swerveAutoBuilder = new SwerveAutoBuilder(m_swerve);
-    // SmartDashboard.putData("calibrate Swerve Modules", new ResetSwerveModules(m_swerve).ignoringDisable(true));
-    // SmartDashboard.putData("reset Odometry",
-    // new InstantCommand(() -> m_swerve.resetOdometry(new Pose2d())).ignoringDisable(true));
-    m_swerve.setDefaultCommand(new TeleopDrive(m_swerve, m_vision, controller, controller.L1(), controller.touchpad(), controller.options(), controller.R1()));
-    // controller.L1().onTrue(new RotateToTarget(m_swerve, m_vision, controller));
-    // sysid = new PhoneixSysid(Constants.sysidConfiguration, m_shooter);
-    // controller.circle().onTrue(sysid.runFullCharacterization(true));
-    
-    //arm
-    SmartDashboard.putData("Change Arm  NeutralMode", new ArmChangeNeutralMode(m_arm).ignoringDisable(true));
-    SmartDashboard.putData("reset arm", new InstantCommand(() -> m_arm.resetPosition(9.57)).ignoringDisable(true));
-    controller.circle().onTrue(new InstantCommand(() -> m_arm.setMotionMagicPosition(90)));
-    controller.square().onTrue(new InstantCommand(() -> m_arm.setMotionMagicPosition(45)));
-    controller.PS().onTrue(new InstantCommand(() -> m_arm.setMotionMagicPosition(Constants.arm.configuration.homePosition)));
-    controller.povDown().onTrue(new ArmHoming(m_arm));
-    // shooter
-    controller.triangle().onTrue(new InstantCommand(() -> m_shooter.setMotionMagicVelocity(6000)));
-    controller.cross().onTrue(new InstantCommand(() -> m_shooter.disableMotors()));
-    //intake
-    controller.R2().onTrue(new InstantCommand(() -> m_intakeSub.setMotorPrecent(-0.4)));
-    controller.L2().onTrue(new InstantCommand(() -> m_intakeSub.setMotorPrecent(0.4)));
-
-    SmartDashboard.putData("presetCheak", new Preset(m_shooter, m_arm, 20, 6000));
+    m_vision = new VisionSubsystem(new CameraSettings(0.30821, 0, 0.10689, 0, 15.13, 0, true), new CameraSettings(0, 0, 0, 0, 0, 0, false));
+    swerveAutoBuilder = new SwerveAutoBuilder(m_swerve);
+    SwerveBinding();
+    armBinding();
+    shooterBinding();
+    intakeBinding();
+    presets();
+    resets();
   }
 
   public void disableMotors() {
@@ -84,10 +73,44 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return null;
+    return swerveAutoBuilder.getAuto();
   }
 
+  public void SwerveBinding(){
+    SmartDashboard.putData("calibrate Swerve Modules", new ResetSwerveModules(m_swerve).ignoringDisable(true));
+    SmartDashboard.putData("reset Odometry", new InstantCommand(() -> m_swerve.resetOdometry(new Pose2d())).ignoringDisable(true));
+    m_swerve.setDefaultCommand(new TeleopDrive(m_swerve, m_vision, drivercontroller, drivercontroller.R2(), drivercontroller.create(), drivercontroller.options(), drivercontroller.R1()));
+  }
+
+  public void armBinding(){
+    SmartDashboard.putData("Change Arm  NeutralMode", new ArmChangeNeutralMode(m_arm).ignoringDisable(true));
+    SmartDashboard.putData("reset arm", new InstantCommand(() -> m_arm.resetPosition(9.57)).ignoringDisable(true));
+    drivercontroller.circle().onTrue(new InstantCommand(() -> m_arm.setMotionMagicPosition(90)));
+    drivercontroller.square().onTrue(new InstantCommand(() -> m_arm.setMotionMagicPosition(45)));
+    drivercontroller.PS().onTrue(new InstantCommand(() -> m_arm.setMotionMagicPosition(Constants.arm.configuration.homePosition)));
+    drivercontroller.povDown().onTrue(new ArmHoming(m_arm));
+  }
+
+  public void shooterBinding(){
+    drivercontroller.triangle().onTrue(new InstantCommand(() -> m_shooter.setMotionMagicVelocity(6000)));
+    drivercontroller.cross().onTrue(new InstantCommand(() -> m_shooter.disableMotors()));
+  }
+
+
   public void intakeBinding(){
-    // controller.L2().onTrue(new IntakeEatNote(m_intakeSub));
+    drivercontroller.R2().onTrue(new InstantCommand(() -> m_intakeSub.setMotorPrecent(-0.4)));
+    drivercontroller.L2().onTrue(new InstantCommand(() -> m_intakeSub.setMotorPrecent(0.4)));
+    operatorController.L2().onTrue(new IntakeEatNote(m_intakeSub));
     }
+
+  public void presets(){
+    driverStationController.LeftBlue().onTrue(new Preset(m_shooter, m_arm, 20, 6000));
+    driverStationController.RightYellow().onTrue(new Preset(m_shooter, m_arm, 40, 8000));
+    driverStationController.DownYellow().onTrue(new Preset(m_shooter, m_arm, 60, 7000));
+    driverStationController.UpBlue().onTrue(new Preset(m_shooter, m_arm, 70, 5000));
+  }
+
+  public void resets(){
+    operatorController.L1().onTrue(new Preset(m_shooter, m_arm, 9.57, 0));
+  }
 }
